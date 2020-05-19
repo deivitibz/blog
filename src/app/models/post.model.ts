ff741384f40cee7fb58773f17537580897a6274b
David Perez <rpprec@gmail.com>
2020-05-19 22:05:22 +0200

    añadido modelo

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,22 @@
+export class Post {
+
+  titulo: string;
+  texto: string;
+  autor: string;
+  imagen: string;
+  fecha: Date;
+  categoria: string;
+
+  constructor(titulo: string, texto: string, autor: string, imagen: string, fecha: Date, categoria: string, ) {
+
+    this.titulo = titulo;
+    this.texto = texto;
+    this.autor = autor;
+    this. imagen = imagen;
+    this.fecha = fecha;
+    this.categoria = categoria;
+
+  }
+
+
+}
